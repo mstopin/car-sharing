e4d7413bf39b10da8e877dc49c195b686e2f1d51
Mateusz Stopiński <46541569+mstopin@users.noreply.github.com>
2023-11-21 09:48:58 +0100

    feat: handle renter events in car aggregate

--- src/main/java/mstopin/carsharing/carsharing/car/domain/AvailableCar.java
+++ src/main/java/mstopin/carsharing/carsharing/car/domain/AvailableCar.java
@@ -1,9 +1,15 @@
 package mstopin.carsharing.carsharing.car.domain;
 
+import mstopin.carsharing.carsharing.renter.domain.events.CarReservedEvent;
+
 import java.util.UUID;
 
 public class AvailableCar extends Car {
   public AvailableCar(UUID carId, Fuel fuel) {
     super(carId, fuel);
+  }
+
+  public ReservedCar handleCarReserved(CarReservedEvent event) {
+    return new ReservedCar(carId, fuel);
   }
 }
--- src/main/java/mstopin/carsharing/carsharing/car/domain/RentedCar.java
+++ src/main/java/mstopin/carsharing/carsharing/car/domain/RentedCar.java
@@ -1,9 +1,15 @@
 package mstopin.carsharing.carsharing.car.domain;
 
+import mstopin.carsharing.carsharing.renter.domain.events.CarRentalFinishedEvent;
+
 import java.util.UUID;
 
 public class RentedCar extends Car {
   public RentedCar(UUID carId, Fuel fuel) {
     super(carId, fuel);
+  }
+
+  public AvailableCar handleCarRentalFinished(CarRentalFinishedEvent event) {
+    return new AvailableCar(carId, fuel);
   }
 }
