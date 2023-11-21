a9d30ea5627e6acce1e8ca84a17593660e106b0f
Mateusz Stopiński <46541569+mstopin@users.noreply.github.com>
2023-11-21 23:02:28 +0100

    test: vehicle service

---
+++ src/test/java/mstopin/carsharing/vehicles/VehicleServiceTest.java
@@ -0,0 +1,38 @@
+package mstopin.carsharing.vehicles;
+
+import mstopin.carsharing.common.domain.DomainEvents;
+import mstopin.carsharing.vehicles.dto.CreateVehicleDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class VehicleServiceTest {
+  @Mock
+  private DomainEvents domainEvents;
+
+  @Mock
+  private VehicleRepository vehicleRepository;
+
+  @InjectMocks
+  private VehicleService vehicleService;
+
+  @Test
+  void shouldAddNewVehicle() {
+    Vehicle vehicle = vehicleService.createVehicle(
+      new CreateVehicleDto("Honda", "Civic")
+    );
+
+    assertThat(vehicle.getVehicleInformation().getMake()).isEqualTo("Honda");
+    assertThat(vehicle.getVehicleInformation().getModel()).isEqualTo("Civic");
+    verify(domainEvents).publish(any(NewVehicleAddedEvent.class));
+  }
+}
