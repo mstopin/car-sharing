e1519349fd67b0717dc2170c1a0aafbc55b13069
Mateusz Stopiński <46541569+mstopin@users.noreply.github.com>
2023-11-22 14:18:49 +0100

    test: add reservation test

---
+++ src/test/java/mstopin/carsharing/carsharing/renter/domain/ReservationTest.java
@@ -0,0 +1,51 @@
+package mstopin.carsharing.carsharing.renter.domain;
+
+import mstopin.carsharing.carsharing.car.domain.AvailableCar;
+import mstopin.carsharing.carsharing.car.domain.Car;
+import mstopin.carsharing.carsharing.car.domain.Fuel;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationTest {
+  @Test
+  void shouldBeExpiredWhenReservationExpirationIsExpired() {
+    Reservation reservation = new Reservation(
+      UUID.randomUUID(),
+      UUID.randomUUID(),
+      new ReservationExpiration(Instant.now().minusSeconds(3600))
+    );
+
+    assertThat(reservation.isExpired()).isEqualTo(true);
+  }
+
+  @Test
+  void shouldNotBeExpiredWhenReservationExpirationIsNotExpired() {
+    Reservation reservation = new Reservation(
+      UUID.randomUUID(),
+      UUID.randomUUID(),
+      new ReservationExpiration(Instant.now().plusSeconds(3600))
+    );
+
+    assertThat(reservation.isExpired()).isEqualTo(false);
+  }
+
+  @Test
+  void shouldReturnIsFor() {
+    UUID carId = UUID.randomUUID();
+    Car car = new AvailableCar(carId, Fuel.fromPercent(100));
+
+    Reservation reservation = new Reservation(
+      UUID.randomUUID(),
+      carId,
+      new ReservationExpiration(Instant.now().plusSeconds(3600))
+    );
+
+    assertThat(reservation.isFor(car)).isEqualTo(true);
+  }
+}
